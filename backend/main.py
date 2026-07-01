dc056975a7872cf8d6c825163f7b31d42f4ff5f5
Navaneethan <navaniram13@gmail.com>
2026-07-02 03:23:09 +0530

    Auto-sync: file changes

--- backend/main.py
+++ backend/main.py
@@ -652,4 +652,4 @@ def submit_application(data: ApplicationSubmissionSchema, conn=Depends(get_db)):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)
